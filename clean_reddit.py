067c2577301a3965f0bf99ef668370fdcdb60ba2
tbuscaglia <tebuscaglia@gmail.com>
2023-10-13 03:01:35 -0300

    Update clean_reddit.py

--- clean_reddit.py
+++ clean_reddit.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 import csv
 import re
 import nltk 
+import os
 
 
 nltk.download('punkt')
@@ -11,7 +12,8 @@ from nltk.tokenize import word_tokenize
 
 
 
-# Open the JSON file for reading
+# Import raw data. Store cleaned and filtered data in filtered_data.
+
 filtered_data = []
 
 with open('C:/Users/tomas/Downloads/MCFC_comments', 'r', encoding="utf8") as file:
@@ -26,15 +28,63 @@ with open('C:/Users/tomas/Downloads/MCFC_comments', 'r', encoding="utf8") as fil
             filtered_data.append([created_utc,data['body']])
 
 
-#filter_test = filtered_data
+# Further cleaning of data. Eg: removing deleted comments. 
+
+#filter_test = filtered_data #Comment this line for production and change below
 
 filtered_data = [sublist for sublist in filtered_data if '[deleted]' not in sublist and '[removed]' not in sublist]
 
-with open('C:/Users/tomas/Documents/UdeSA/Tercer Año/Segundo Cuatri/NLP/NLP-Proyecto/clean_data/mcfc_filtered.csv', mode='w', encoding="utf-8") as _file:
-    _writer = csv.writer(_file, delimiter=',')
-    for data in filtered_data:
-        _writer.writerow(data)
+subset_filtered_data = filtered_data[:1000]
 
+### Clean data here ###
+
+
+# Once we have cleaned all of our data we write a new csv file to store it. 
+
+with open('C:/Users/tomas/Documents/UdeSA/Tercer Año/Segundo Cuatri/NLP/NLP-Proyecto/clean_data/mcfc_filtered.csv', mode='w', newline='', encoding="utf-8") as file:
+    writer = csv.writer(file, delimiter='\t')  # Use a tab as the delimiter
+
+    # Write the header row (optional)
+    writer.writerow(["Unix Date", "Comment"])
+
+    # Write each data row
+    for row in filtered_data:
+        writer.writerow([row[0], row[1]])
+
+# Reading data from the CSV file while preserving paragraph structure
+mcfc_clean = []
+
+with open('C:/Users/tomas/Documents/UdeSA/Tercer Año/Segundo Cuatri/NLP/NLP-Proyecto/clean_data/mcfc_filtered.csv', mode='r', encoding="utf-8") as file:
+    reader = csv.reader(file, delimiter='\t')  # Specify the same delimiter
+
+    # Skip the header row (if present)
+    next(reader, None)
+
+    for row in reader:
+        unix_time = int(row[0])
+        text = row[1]
+        mcfc_clean.append([unix_time, text])
+
+subset_clean_mcfc = mcfc_clean[:1000] 
+
+# Delete raw data to save storage
+
+file_path = 'C:/Users/tomas/Downloads/MCFC_comments'
+
+if os.path.exists(file_path):
+    os.remove(file_path)
+    print(f"The file '{file_path}' has been deleted.")
+else:
+    print(f"The file '{file_path}' does not exist.")
+
+
+
+
+'''    
+#Remove any empty lists              
+#mcfc_clean = [sublist for sublist in mcfc_clean if any(element.strip() for element in sublist)]
+'''
+ 
 
 '''
 count = 0
