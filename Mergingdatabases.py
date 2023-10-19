a14e8fd5cda6b22d1db1abb8e139de792d659c04
tbuscaglia <tebuscaglia@gmail.com>
2023-10-19 20:44:58 -0300

    Update Mergingdatabases.py

--- Mergingdatabases.py
+++ Mergingdatabases.py
@@ -1,5 +1,6 @@
 import csv
 import pandas as pd
+import math
 #import numpy as np
 
 data538 = []
@@ -20,17 +21,6 @@ selected_columns = [1, 4, 5, 8, 9, 10, 13, 14, 15, 16]
 
 data538_3 = [[row[i] for i in selected_columns] for row in data538_2]
 
-
-'''
-with open('clean538.csv', 'w', newline='') as file_csv:
-    csv_writer = csv.writer(file_csv)
-    for i in range(len(data538_3)):
-        date = data538_3[i][0]
-        team1 = data538_3[i][1]
-        team2 = data538_3[i][2]    
-        csv_writer.writerow([date, team1, team2])
-'''
-
 dataEPL21_22 = []
 
 with open('C:/Users/tomas/Downloads/E0.csv', 'r') as csv_file:
@@ -38,7 +28,6 @@ with open('C:/Users/tomas/Downloads/E0.csv', 'r') as csv_file:
     rowcount = 0
     for row in csv_reader:
       dataEPL21_22.append(row)
-
 
 # Transform data into dataframes: 
 
@@ -63,13 +52,6 @@ new_name = ['Man United', 'Leicester', 'Norwich', 'Tottenham', 'Leeds',
 
 df_spi['HomeTeam'] = df_spi['HomeTeam'].replace(name_to_find, new_name)
 df_spi['AwayTeam'] = df_spi['AwayTeam'].replace(name_to_find, new_name)
-
-'''
-unique_values_bets = df_bets['HomeTeam'].unique()
-
-# Get unique values from the 'HomeTeam' column in df_spi
-unique_values_spi = df_spi['HomeTeam'].unique()
-'''
 
 result = df_bets.merge(df_spi, on=['Date', 'HomeTeam', 'AwayTeam'], how='inner')
 
@@ -97,12 +79,10 @@ match_info['beta_away'] = 0.2 * match_info['R_away']
 
 match_info['prob_awaywin'] = match_info['prob_awaywin'].astype(float)
 match_info['prob_homewin'] = match_info['prob_homewin'].astype(float)
+match_info['importance_home'] = match_info['importance_home'].astype(float)
+match_info['importance_away'] = match_info['importance_away'].astype(float)
 match_info['score_home'] = match_info['score_home'].astype(int)
 match_info['score_away'] = match_info['score_away'].astype(int)
-
-
-unique_values = match_info['HomeTeam'].unique()
-
 
 #Offensive and defensive scores 
 
@@ -112,7 +92,6 @@ team_scores = [['Man City', 3.0, 0.2], ['Liverpool', 3.0, 0.4], ['Chelsea', 2.5,
          ['Crystal Palace', 1.9, 0.8], ['Brentford', 1.8, 0.8], ['Everton', 1.9, 0.9],
          ['Southampton', 1.8, 0.9], ['Leeds',1.9, 1.0], ['Burnley',1.8, 1.0], ['Watford', 1.7, 1.1],
          ['Newcastle', 1.7, 1.1], ['Norwich', 1.5, 1.1]]
-
 
 team_scores = pd.DataFrame(team_scores, columns=['Team', 'OffensiveScore', 'DefensiveScore'])
 
@@ -141,15 +120,11 @@ match_info['def_away'] = defensive_scores_away
 match_info['gamma_home'] = (match_info['prob_awaywin'] * match_info['off_home'] - match_info['prob_homewin'] * match_info['def_home']) / (match_info['off_home'] + match_info['def_home'])
 match_info['gamma_away'] = (match_info['prob_homewin'] * match_info['off_away'] - match_info['prob_awaywin'] * match_info['def_away']) / (match_info['off_away'] + match_info['def_away'])
 
-'''
-queda crear el mu_home y mu_away
 
-despues hacer el gap score
+match_info['mu_home'] = match_info['importance_home'].apply(lambda x: math.exp(x/100 - 1))
+match_info['mu_away'] = match_info['importance_away'].apply(lambda x: math.exp(x/100 - 1))
 
-'''
-
-
-
-
+match_info['gap_score_home'] = match_info['mu_home'] * (match_info['gamma_home'] + match_info['beta_home'])
+match_info['gap_score_away'] = match_info['mu_away'] * (match_info['gamma_away'] + match_info['beta_away'])
 
 
