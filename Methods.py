35792cf5ed0cba51484247ec9bffe5fdefdb5e8b
Juliandelsman <142118069+Juliandelsman@users.noreply.github.com>
2023-10-30 11:15:12 -0300

    Update Methods.py

--- Methods.py
+++ Methods.py
@@ -3,17 +3,18 @@ from tqdm import tqdm
 import numpy as np
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 tqdm.pandas()
+
 team_names = ['Man United', 'Chelsea', 'Everton', 'Leicester',
   'Norwich', 'Newcastle', 'Tottenham', 'Liverpool', 'Aston Villa',
  'Crystal Palace', 'Leeds', 'Man City', 'Brighton', 'Southampton', 'Wolves',
  'Arsenal', 'West Ham']
 team_data = {}
 for team in tqdm(team_names):
-    file_path = f"C:\Users\tomas\Documents\UdeSA\Tercer Año\Segundo Cuatri\NLP\Datos Proyecto\Datos limpios\processed comments\Crystal Palace.csv"{team}.csv"
+    file_path = f"/Users/julianandelsman/Desktop/NLP/Final project/Data/{team}.csv"
     df_team = pd.read_csv(file_path, sep='\t')
     team_data[team] = df_team
     
-MatchInfo = pd.read_csv("C:/Users/tomas/Downloads/MatchInfo.csv")
+MatchInfo = pd.read_csv('/Users/julianandelsman/Desktop/NLP/Final project/Data/MatchInfo.csv')
 
 ##VADER##
 # Start VADER
@@ -32,10 +33,117 @@ def analyze_sentiment(text):
             return 'Neutral'
     else:
         return 'Neutral'
-    
-for team in team_names:
-    team_data[team]['sentiments'] = team_data[team]['Comment'].progress_apply(analyze_sentiment)
 
+#Defining Quantile separation function
+match_info_vader = MatchInfo[['HomeTeam', 'AwayTeam','Unix_start', 'Unix_end', 'gap_score_home','gap_score_away']]
+
+team_played = {}
+
+team_gap = {}
+for index, row in tqdm(match_info_vader.iterrows()):
+    if row['HomeTeam'] not in ['Brentford', 'Watford', 'Burnley']:
+        team = row['HomeTeam']
+        gap_score = row['gap_score_home']
+        cuantil = gap_score
+        match_time = row['Unix_end']
+        if team not in team_gap:
+            team_gap[team] = {}
+        team_gap[team][match_time] = cuantil
+        if team not in team_played:
+            team_played[team] = []
+        team_played[team].append(match_time)
+    if row['AwayTeam'] not in ['Brentford', 'Watford', 'Burnley']:
+        team = row['AwayTeam']
+        gap_score = row['gap_score_away']
+        cuantil = gap_score
+        match_time = row['Unix_end']
+        if team not in team_gap:
+            team_gap[team] = {}        
+        team_gap[team][match_time] = cuantil        
+        if team not in team_played:
+            team_played[team] = []
+        team_played[team].append(match_time)
+
+
+data_team_time = {}
+for team in team_played:
+    data_team_time[team] = {}
+    match_starts = []
+    for match_time in team_played[team]:
+        match_starts.append([match_time,match_time+21600])
+        data_team_time[team][match_time] = []
+    for idx_2,row_2 in tqdm(team_data[team].iterrows()):  #This runs 18 times instead of 600 times
+        if any(item[0] < row_2['Unix Date'] <= item[1] for item in match_starts):
+            # Find the corresponding match start time for the comment
+            comment_start = next(item[0] for item in match_starts if item[0] <= row_2['Unix Date'] <= item[1])
+            data_team_time[team][comment_start].append(row_2['Comment'])
+
+Pom_sentiment = {}
+for team in team_names:
+    #Pom_sentiment[team] = {}
+    for time in team_played[team]:
+        Pom_sentiment[team][time] = {}
+        compound_sum = 0
+        for i in range (len(data_team_time[team][time])):
+            if isinstance(data_team_time[team][time][i], str):
+                compound_sum += analyzer.polarity_scores(data_team_time[team][time][i])['compound']
+        Pom_sentiment[team][time] = compound_sum/len(data_team_time[team][time])
+print(team_played['Man United'])
+
+#---------#
+
+data_team_time_pre = {}
+for team in team_played:
+    data_team_time_pre[team] = {}
+    match_starts = []
+    for match_time in team_played[team]:
+        match_starts.append([match_time - 28800,match_time - 7200])
+        data_team_time_pre[team][match_time] = []
+    for idx_2,row_2 in tqdm(team_data[team].iterrows()):  #This runs 18 times instead of 600 times
+        if any(item[0] < row_2['Unix Date'] <= item[1] for item in match_starts):
+            # Find the corresponding match start time for the comment
+            comment_start = next(item[1]+7200 for item in match_starts if item[0] <= row_2['Unix Date'] <= item[1])
+            data_team_time_pre[team][comment_start].append(row_2['Comment'])
+
+Prem_sentiment = {}
+for team in team_names:
+    Prem_sentiment[team] = {}
+    for time in team_played[team]:
+        #Prem_sentiment[team][time] = {}
+        compound_sum = 0
+        for i in range (len(data_team_time_pre[team][time])):
+            if isinstance(data_team_time_pre[team][time][i], str):
+                compound_sum += analyzer.polarity_scores(data_team_time_pre[team][time][i])['compound']
+        if len(data_team_time_pre[team][time]) == 0:
+            Prem_sentiment[team][time] = []
+        if len(data_team_time_pre[team][time])>0:
+            Prem_sentiment[team][time] = compound_sum/len(data_team_time_pre[team][time])
+
+print(Prem_sentiment['Norwich'][1652301900])
+print(team_gap['Leicester'][1646487000])
+sent_change = {}
+for team in team_names:
+    sent_change[team] = {}
+    for time in team_played[team]:
+        gap = team_gap[team][time]
+        sent_change[team][time]= {}
+        sent_change[team][time][gap] = {}
+        if len(data_team_time_pre[team][time]) > 0 and len(data_team_time[team][time])>0:
+            if Prem_sentiment[team][time] != 0:
+                sent_change[team][time][gap] = ((Pom_sentiment[team][time] - Prem_sentiment[team][time])/Prem_sentiment[team][time])*100
+
+
+list= []
+for team, time_data in sent_change.items():
+    for time, gap_data in time_data.items():
+        for gap, value in gap_data.items():
+            list.append((team, time, gap, value))
+
+SentChange = pd.DataFrame(list, columns=['Team', 'Match End', 'Gap Score', 'Percentage change in compound score'])
+SentChange.to_csv("/Users/julianandelsman/Desktop/NLP/Final project/Data/Sentchange.csv", index=True)
+
+print(SentChange.head())
+'''
 def positive_calc(team, time):
     positive_count = 0
     total_count = 0
@@ -55,7 +163,6 @@ def positive_calc(team, time):
 
 MatchInfo['Local_positive'] = MatchInfo.progress_apply(lambda row: positive_calc(row['HomeTeam'], row['Unix_end']), axis=1)
 MatchInfo['Away_positive'] = MatchInfo.progress_apply(lambda row: positive_calc(row['AwayTeam'], row['Unix_end']), axis=1)
-
 def negative_calc(team, time):
     negative_count = 0
     total_count = 0
@@ -78,172 +185,6 @@ print(MatchInfo.head())
 MatchInfo['Local_neutral'] = 100 - MatchInfo['Local_positive'] - MatchInfo['Local_negative']
 MatchInfo['Away_neutral'] = 100 - MatchInfo['Away_positive'] - MatchInfo['Away_negative']
 MatchInfo.to_csv("/Users/julianandelsman/Desktop/NLP/Final project/Data/MatchInfo.csv", index=False)
+'''
 
 #------------------------------------------------------------------------------
-## TF-IDF ##
-
-import nltk
-nltk.download('stopwords')
-from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize
-from sklearn.feature_extraction.text import TfidfVectorizer
-from wordcloud import WordCloud
-import matplotlib.pyplot as plt
-
-nltk.download('punkt')
-nltk.download('wordnet')
-
-english_stop_words = set(stopwords.words('english'))
-
-# Load the CSV file with your song data (replace with your data file)
-
-match_info_tfidf = pd.read_csv("C:/Users/tomas/Documents/UdeSA/Tercer Año/Segundo Cuatri/NLP/Final Project/MatchInfo.csv")
-
-#'''
-team_names = ['Man United', 'Chelsea', 'Everton', 'Leicester',
-  'Norwich', 'Newcastle', 'Tottenham', 'Liverpool', 'Aston Villa',
- 'Crystal Palace', 'Leeds', 'Man City', 'Brighton', 'Southampton', 'Wolves',
- 'Arsenal', 'West Ham']
-team_data = {}
-for team in tqdm(team_names):
-    file_path = f"C:/Users/tomas/Documents/UdeSA/Tercer Año/Segundo Cuatri/NLP/Datos Proyecto/Datos limpios/{team}.csv"
-    df_team = pd.read_csv(file_path, sep='\t')
-    team_data[team] = df_team
-
-#'''
-# Initialize the TF-IDF vectorizer for text
-
-tfidf_vectorizer = TfidfVectorizer(max_df=0.85, max_features=1000, stop_words=english_stop_words)
-
-# Lemmatize and preprocess the lyrics data
-lemmatizer = WordNetLemmatizer()
-
-'''
-tfidf_data = {}
-for artist in data:
-    tfidf_data[artist] = ' '.join([lemmatizer.lemmatize(word.lower()) for lyrics in data[artist] for word in word_tokenize(lyrics) if word.isalpha()])
-'''
-
-def comment_processing(text):
-    processed_text = ""
-    if isinstance(text, str): 
-        text = text.lower()
-        words = nltk.word_tokenize(text)
-        stop_words = set(stopwords.words('english'))
-        words = [word for word in words if word not in stop_words]
-        words = [lemmatizer.lemmatize(word) for word in words]
-        processed_text = ' '.join(words)
-    return processed_text
-
-Leicester = team_data['Leicester']
-
-Leicester['Processed Comment'] = Leicester['Comment'].apply(comment_processing)
-
-
-for team in team_names:
-    team_data[team]['Processed Comment'] = team_data[team]['Comment'].progress_apply(comment_processing)
-
-
-output_directory = "C:/Users/tomas/Documents/UdeSA/Tercer Año/Segundo Cuatri/NLP/Datos Proyecto/Datos limpios/processed comments/"
-
-# Iterate through each team
-for team in tqdm(team_names):
-    team_df = team_data[team]
-    
-    # Define the output file path for this team
-    output_file_path = output_directory + f"{team}.csv"
-    
-    # Save the entire DataFrame to a CSV file
-    team_df.to_csv(output_file_path, index=False, encoding='utf-8')
-
-
-#tfidf_matrix = tfidf_vectorizer.fit_transform(Leicester['Processed Comment'])
-
-match_info_tfidf = match_info_tfidf[['HomeTeam', 'AwayTeam', 'Unix_end', 'gap_score_home','gap_score_away']]
-
-quantiles = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
-
-gap_scores_combined = np.concatenate([match_info_tfidf['gap_score_home'], match_info_tfidf['gap_score_away']])
-
-cuantiles = np.quantile(gap_scores_combined, quantiles)
-
-def encontrar_cuantil(gapscore):
-    for i in range(len(cuantiles)):
-        if gapscore <= cuantiles[i]:
-            return i
-    return len(cuantiles) - 1
-
-#print(encontrar_cuantil(0.15))
-comentarios_cuantil = {}
-
-def cuantile_text(team, time, gap):
-    cuantil = encontrar_cuantil(gap)
-    if cuantil not in comentarios_cuantil:
-        comentarios_cuantil[cuantil] = [] 
-    comentarios = []
-    for index, row in team_data[team].iterrows():      
-        if row['Unix Date'] > time and row['Unix Date'] < (time + 21600):
-            palabras = [palabra for palabra in row['Processed Comment'].split() if palabra.isalpha()]
-            comentario_filtrado = ' '.join(palabras)
-            comentarios.append(comentario_filtrado)  
-    comentarios_cuantil[cuantil].append(' '.join(comentarios))
-    return comentarios_cuantil            
- 
-     
-for index, row in tqdm(match_info_tfidf.iterrows()):
-    if row['HomeTeam'] not in ['Brentford', 'Watford', 'Burnley']:
-        cuantile_text(row['HomeTeam'], row['Unix_end'], row['gap_score_home'])
-    if row['AwayTeam'] not in ['Brentford', 'Watford', 'Burnley']:
-        cuantile_text(row['AwayTeam'], row['Unix_end'], row['gap_score_away'])
-        
-
-
-
-
-
-
-
-
-
-'''
-def postmatch_tfidf(team, time, team_data, tfidf_vectorizer):
-    if team in ['Brentford', 'Watford', 'Burnley']:
-        return None
-    else:
-        comments = team_data[team][(team_data[team]['Unix Date'] > time) & (team_data[team]['Unix Date'] < (time + 21600))]['Processed Comment']
-        tfidf_matrix = tfidf_vectorizer.fit_transform(comments)
-        return tfidf_matrix
-
-
-def postmatch_tfidf(team, time):
-    if team in ['Brentford', 'Watford', 'Burnley']:
-        return None
-    else: 
-        for index, row in team_data[team].iterrows(): 
-            if row['Unix Date']> time and row['Unix Date']< (time + 21600):
-                tfidf_matrix = tfidf_vectorizer.fit_transform('')
- 
-  '''  
-
-
-#tfidf_matrix = tfidf_vectorizer.fit_transform(match_info_tfidf['processed_text'])
-
-'''
-# Perform TF-IDF analysis
-tfidf_matrix = tfidf_vectorizer.fit_transform(tfidf_data.values())
-tfidf_feature_names = tfidf_vectorizer.get_feature_names_out()
-tfidf_df = pd.DataFrame(data=tfidf_matrix.toarray(), columns=tfidf_feature_names, index=tfidf_data.keys())
-'''
-# Create a WordCloud from aggregated TF-IDF values
-agg_tfidf = tfidf_df.sum().sort_values(ascending=False)
-wordcloud = WordCloud(width=800, height=400, background_color='white').generate_from_frequencies(agg_tfidf)
-
-# Display the WordCloud
-plt.figure(figsize=(10, 5))
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis('off')
-plt.title("TF-IDF Values for english songs")
-wordcloud.to_file('wordcloud_en.png')
-plt.show()
-
